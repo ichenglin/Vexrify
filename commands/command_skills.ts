88d277654e3242588bec78a98c1e5c967226f7c1
ichenglin <fireclaws9@gmail.com>
2023-07-31 13:16:14 +0800

    Added Top Percentage in Skills

--- commands/command_skills.ts
+++ commands/command_skills.ts
@@ -65,9 +65,9 @@ export default class SkillsCommand extends VerificationCommand {
             .addFields(
                 ...team_season_skills_sorted.map((skill_data) => (
                     {name: `🏁 ${skill_data.season_data.season_name} 🏁`, value: [
-                        `▪️ Rank: \`#${skill_data.season_skills.skills_rank}\` out of ${skill_data.season_skills.skills_entries} entries`,
-                        `▪️ Driver: \`${skill_data.season_skills.skills_score.driver_score}\``,
-                        `▪️ Programming: \`${skill_data.season_skills.skills_score.programming_score}\``
+                        `<:vrc_dot_blue:1135437387619639316> Rank: \`#${skill_data.season_skills.skills_rank}\` out of ${skill_data.season_skills.skills_entries} entries (\`Top ${Math.ceil(skill_data.season_skills.skills_rank / skill_data.season_skills.skills_entries * 100)}%\`)`,
+                        `<:vrc_dot_blue:1135437387619639316> Driver: \`${skill_data.season_skills.skills_score.driver_score}\``,
+                        `<:vrc_dot_blue:1135437387619639316> Programming: \`${skill_data.season_skills.skills_score.programming_score}\``
                     ].join("\n")}
                 )))
             .setImage("attachment://skills_graph.png")
